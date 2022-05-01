0eb228d6792844bde7dac8916ac677f76dd446f0
Daulet <moni_i@mail.ru>
2022-05-02 05:29:18 +0600

    Начал 1 задачу

---
+++ Dom_zadania4/Zadania_001/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 1
+// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// A в степене B
+
+// Для того, чтобы возвести в степень, нужно вызвать метод Pow, который находится в статическом классе, поэтому необходимо:
+// Math.Pow(x, stepen);
+// Обратите внимание, что метод принимает значения типа double, если мы объявим тип int, то ошибки не будет:
+// int x = 5;
+// int stepen = 4;
+// Console.WriteLine("5^4 = " + Math.Pow(x, stepen));
+// Если вам нужно получить целое число степени, то можете сделать так:
+// int i = Convert.ToInt32(Math.Pow(5, 3));
+// Если мы напишем:
+// int i = Math.Pow(5, 3);
+// То будет ошибка, поэтому нужно конвертация в целое число.
+
+int DlinMass = 10;
+int[] a = new int[DlinMass];
+Console.Write("Массив: ");
+for (int i = 0; i < a.Length; i++)
+{
+    Console.Write("{0} ", i + 1);
+}
+Console.WriteLine();
+
+
+
+
+// int x = 5;
+// int stepen = 4;
+// Console.WriteLine("5^4 = " + Math.Pow(x, stepen));
+
+// int i = Convert.ToInt32(Math.Pow(x, stepen));
+// Console.WriteLine(i);
+
+
+// int[] nums = new int[stepen];                   // создаем массив с количеством stepen
+
+// int[] numbers = { 1, 2, 3, 4, 5 };
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     numbers[i] = numbers[i] * 2;
+//     Console.WriteLine(numbers[i]);
+// }
